ca515f5fe04b6aa3fca5e6284d0dfb12f6e40ffa
NelsonBlakeN <nelson.blaken@gmail.com>
2019-03-21 12:58:01 -0500

    Rearragne some structure for more clear error handling.
    In this commit, the setup script was removed becasue of errors related to X server initialziation and activating the venv. The single package is now installed in the base environment

--- keyboard_heatmap.py
+++ keyboard_heatmap.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python3
 '''Create a heatmap of the keys used over a period of time'''
-import json
-from collections import defaultdict
-from pynput.keyboard import Key, KeyCode, Listener
-
-# Create static dict
-with open('/home/blake/Dropbox/Projects/KeyboardHeatmap/all_time.json') as file:
-    ALL_TIME = json.load(file)
-
-# Create defaultdict from current keys dict
-KEYS = defaultdict(int, ALL_TIME["keys"])
-ALL_TIME["keys"] = KEYS
 
 def on_press(_key):
     '''What to do when a key is pressed'''
@@ -19,16 +8,44 @@ def on_press(_key):
         ALL_TIME["total"] += 1
         ALL_TIME["keys"][key] += 1
     else:
-        key = str(Key(_key.value)).split('.')[1]
+        key = str(Key(_key.value)).split('.')[1]    # Get key name from key
         ALL_TIME["total"] += 1
         ALL_TIME["keys"][key] += 1
 
+def exit_handler():
+    '''Write to file any time the program ends'''
+    with open(DATAFILE, "w") as write_file:
+        # Convert defaultdict back to normal dict
+        ALL_TIME["keys"] = dict(ALL_TIME["keys"])
+        write_file.write(json.dumps(ALL_TIME, sort_keys=True, indent=4, separators=(',', ':')))
+
 try:
+    import atexit
+    import json
+    from collections import defaultdict
+    print("Importing keyboar listener...", end='')
+    from pynput.keyboard import Key, KeyCode, Listener
+    print("complete.")
+
+    DATAFILE = '/home/blake/Dropbox/Projects/KeyboardHeatmap/all_time.json'
+
+    # Create static dict
+    with open(DATAFILE) as file:
+        ALL_TIME = json.load(file)
+
+    # Create defaultdict from current keys dict
+    KEYS = defaultdict(int, ALL_TIME["keys"])
+    ALL_TIME["keys"] = KEYS
+
+    atexit.register(exit_handler)
+
+except RuntimeError as _e:
+    print("Setup failed: {}".format(_e))
+
+try:
+    print("Starting listener")
     with Listener(
             on_press=on_press) as listener:
         listener.join()
 finally:
-    with open('/home/blake/Dropbox/Projects/KeyboardHeatmap/all_time.json', "w") as file:
-        # Convert defaultdict back to normal dict
-        ALL_TIME["keys"] = dict(ALL_TIME["keys"])
-        file.write(json.dumps(ALL_TIME, sort_keys=True, indent=4, separators=(',', ':')))
+    exit_handler()
--- keyboard_heatmap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /home/blake/Py3Envs/kh/bin/activate
-
-export DISPLAY=':1'
-
-/home/blake/Dropbox/Projects/KeyboardHeatmap/keyboard_heatmap.py
